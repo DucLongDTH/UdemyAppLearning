bf5245cd396d07fd597b091e45b362fc042a8296
DucLongDev <long.nguyen2@ntq-solution.com.vn>
2023-01-11 16:11:25 +0700

    test listview

--- app/src/main/java/com/duclongdev/udemytest/ListTaskActivity.kt
+++ app/src/main/java/com/duclongdev/udemytest/ListTaskActivity.kt
@@ -1,4 +1,39 @@
 package com.duclongdev.udemytest
 
-class ListTaskActivity {
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_list_task.*
+
+class ListTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_task)
+        val arrayAdapter: ArrayAdapter<*>
+        val tasks = arrayOf(
+            "App 1",
+            "App 2",
+            "App 3",
+            "App 4",
+            "App 5",
+            "App 6",
+            "App 7",
+            "App 8",
+            "App 9",
+            "App 10",
+            "App 1",
+            "App 2",
+            "App 3",
+            "App 4",
+            "App 5",
+            "App 6",
+            "App 7",
+            "App 8",
+            "App 9",
+            "App 10"
+        )
+        arrayAdapter = ArrayAdapter(this, R.layout.item_list_task, tasks)
+        lv_task.adapter = arrayAdapter
+    }
 }
--- app/src/main/java/com/duclongdev/udemytest/MainActivity.kt
+++ app/src/main/java/com/duclongdev/udemytest/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.duclongdev.udemytest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_go.setOnClickListener{
+            Log.d("LogD","on tap button hi hi")
+            startActivity(Intent(this, ListTaskActivity::class.java))
+        }
     }
 
 }
