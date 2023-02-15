1f19de9eb064991296d49fb7193cb09a9c365883
DucLongDev <long.nguyen2@ntq-solution.com.vn>
2023-02-15 14:21:40 +0700

    update

--- app/src/main/java/com/duclongdev/udemytest/kiddrawingapp/KidDrawingAppMain.kt
+++ app/src/main/java/com/duclongdev/udemytest/kiddrawingapp/KidDrawingAppMain.kt
@@ -1,9 +1,11 @@
 package com.duclongdev.udemytest.kiddrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.duclongdev.udemytest.R
 import com.duclongdev.udemytest.databinding.ActivityKidDrawingAppMainBinding
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class KidDrawingAppMain : AppCompatActivity() {
     private lateinit var binding: ActivityKidDrawingAppMainBinding
@@ -11,6 +13,28 @@ class KidDrawingAppMain : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityKidDrawingAppMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.drPaint.setBrushSize(10.toFloat())
+
+        binding.ibBrushChoose.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
     }
 
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.ib_small_brush.setOnClickListener {
+            binding.drPaint.setBrushSize(10.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.ib_medium_brush.setOnClickListener {
+            binding.drPaint.setBrushSize(20.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.ib_large_brush.setOnClickListener {
+            binding.drPaint.setBrushSize(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
+    }
 }
